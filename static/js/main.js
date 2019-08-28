b569c55461199796b9b5514e68b0b160e159ee8b
Adam Rajmuller <rajfta@gmail.com>
2019-08-28 16:24:26 +0200

    smoothscroll

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,30 @@ function initMap() {
         map: map
     });
 }
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+
+
+    //Scroll Navbar Opacity
+    window.addEventListener("scroll", function(){
+        if (window.scrollY > 150) {
+            document.querySelector("#navbar").style.opacity = 0.85;
+        } else {
+            document.querySelector("#navbar").style.opacity = 1;
+        }
+    })
